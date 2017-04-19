7ae55fb4079090614902246a9687efbb41a063d7
lewei <lewei@DESKTOP-UVA71NO>
2017-04-19 14:01:31 +0800

    initial project

---
+++ gitprac/src/main/java/rabbit/gitprac/App.java
@@ -0,0 +1,13 @@
+package rabbit.gitprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World v1!" );
+    }
+}
